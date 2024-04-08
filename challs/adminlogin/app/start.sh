96fad2c2737c5e59eb5cb90a926a5cbc3a60c067
aJuvan <andraz.juvan@heimdall.si>
2024-04-08 13:21:57 +0200

    Removed unused chall, fixed dockerfiles, fixed challenge files

--- challs/adminlogin/app/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chown pilot:pilot /app/baby_bof
-chmod +x /app/baby_bof
-while true; do
-	su pilot -c 'timeout -k 30 1d socat TCP-LISTEN:5555,nodelay,reuseaddr,fork EXEC:"stdbuf -i0 -o0 -e0 ./baby_bof"'
-done
--- challs/integer/app/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chown pilot:pilot /app/chall
-chmod +x /app/chall
-while true; do
-	su pilot -c 'timeout -k 30 1d socat TCP-LISTEN:5555,nodelay,reuseaddr,fork EXEC:"stdbuf -i0 -o0 -e0 ./baby_bof"'
-done
